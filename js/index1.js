9fac5d566610175f8fda048d51572c71dd2065b2
Richard Villalba <richard.villalba.mendieta@gmail.com>
2022-11-24 08:31:59 -0400

    terminando primera parte

---
+++ js/index1.js
@@ -0,0 +1,12 @@
+function cambioUyuni(){
+
+    location.href="../html/index.html";
+}
+
+function paquetes(){
+    location.href="../html/index2.html"
+}
+
+function inicio(){
+    location.href="../html/index1.html"
+}
