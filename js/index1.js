8e3e672dcb2adf0acd6e23851f1d6101c2680cce
Richard Villalba <richard.villalba.mendieta@gmail.com>
2022-11-29 15:31:56 -0400

    fin de primera pagina

--- js/index1.js
+++ js/index1.js
@@ -4,9 +4,20 @@ function cambioUyuni(){
 }
 
 function paquetes(){
+  
     location.href="../html/index2.html"
 }
 
 function inicio(){
     location.href="../html/index1.html"
+}
+
+const menu=document.getElementById("icon-menu");
+
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click",verMenu)
+
+function verMenu(){
+    alert("hola");
 }
